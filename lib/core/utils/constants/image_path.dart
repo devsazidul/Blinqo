3a08d10a20d55411cdb2b89fa5dac32b385c50df
shakilahammed055 <shakilahammed055@gmail.com>
2025-04-07 09:43:24 +0600

    onboarding complete

--- lib/core/utils/constants/image_path.dart
+++ lib/core/utils/constants/image_path.dart
@@ -4,4 +4,7 @@ class ImagePath {
   static const String serviceprovider01 = 'assets/images/serviceprovider01.png';
   static const String serviceprovider02 = 'assets/images/serviceprovider02.png';
   static const String serviceprovider03 = 'assets/images/serviceprovider03.png';
+  static const String venueonboarding01 = 'assets/images/venue_onboarding1.png';
+  static const String venueonboarding02 = 'assets/images/venue_onboarding2.png';
+  static const String venueonboarding03 = 'assets/images/venue_onboarding3.png';
 }
--- lib/features/role_page/controller/role_controller.dart
+++ lib/features/role_page/controller/role_controller.dart
@@ -1,5 +1,6 @@
 import 'package:blinqo/features/role/event_planner/onboring/home_event_planner.dart';
 import 'package:blinqo/features/role/service_provider/onbording/screen/onbording_screen.dart';
+import 'package:blinqo/features/role/venue_owner/onboarding_screen/screen/venue_onboarding_screen.dart';
 import 'package:get/get.dart';
 
 class RoleController extends GetxController {
@@ -14,7 +15,7 @@ class RoleController extends GetxController {
         Get.to(() => HomeEventPlanner());
         break;
       case 1:
-        Get.to(() => OnbordingScreen());
+        Get.to(() => VenueOnboardingScreen());
         break;
       case 2:
         Get.to(() => OnbordingScreen());
