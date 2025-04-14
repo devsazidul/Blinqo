07db7cc41f556a9cc2ce1b499cbbeab5338e96c5
bdcalling <izaz3531ahmed@gmail.com>
2025-04-14 15:29:36 +0600

    14thapril

---
+++ lib/features/role/venue_owner/myvenue/widget/custom_amenity.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/cupertino.dart';
+
+import '../../../../../core/common/styles/global_text_style.dart';
+
+class CustomAmenityBox extends StatelessWidget {
+  final IconData icon;
+  final String label;
+  final double heights;
+  final double widths;
+
+
+  const CustomAmenityBox({
+    super.key,
+    required this.icon,
+    required this.label,
+    required this.heights, required this.widths,
+
+
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+     height:heights ,
+      width: widths,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(4),
+        border: Border.all(
+          width: 1,
+          color: const Color(0xffC0C0C0),
+        ),
+      ),
+      child: Row(
+        children: [
+          const SizedBox(width: 5),
+          Icon(
+            icon,
+            size: 14,
+            color: const Color(0xffD4AF37),
+          ),
+          const SizedBox(width: 8),
+          Text(
+            label,
+            style: getTextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w400,
+              color: const Color(0xff333333),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
