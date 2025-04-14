7a265a4658f18fa76f7c043e53cd1ca9c3a4a2be
Sazidul islam <rabbiking00@gmail.com>
2025-04-14 18:58:10 +0600

    ..

--- lib/features/role/venue_owner/myvenue/screen/VenueDetailsScreen.dart
+++ lib/features/role/venue_owner/myvenue/screen/VenueDetailsScreen.dart
@@ -420,7 +420,7 @@ class VenueDetailsScreen extends StatelessWidget {
                     ],
                   ),
                   const SizedBox(height: 40),
-                   RevenueCard(),
+                  RevenueCard(),
                   const SizedBox(height: 40),
                   Center(
                     child: Text(
