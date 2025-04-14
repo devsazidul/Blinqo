cfd7318404843a82ce75b537366d3f968737da65
bdcalling <izaz3531ahmed@gmail.com>
2025-04-14 18:35:12 +0600

    that's it for today

--- lib/features/role/venue_owner/myvenue/screen/edit_venue.dart
+++ lib/features/role/venue_owner/myvenue/screen/edit_venue.dart
@@ -1,16 +1,22 @@
 import 'package:blinqo/core/utils/constants/image_path.dart';
+import 'package:blinqo/features/role/venue_owner/myvenue/controller/myview_controller.dart';
+import 'package:blinqo/features/role/venue_owner/myvenue/widget/custom_shape.dart';
 import 'package:flutter/material.dart';
-
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 import '../../../../../core/common/styles/global_text_style.dart';
 import '../../../../../core/utils/constants/colors.dart';
 import '../../../../../core/utils/constants/icon_path.dart';
-
+import '../../overview/controller/overview_controller.dart';
+import '../widget/custom_textfield.dart';
+import '../widget/facilty_Tag.dart';
 class EditVenue extends StatelessWidget {
-  final String image;
-  const EditVenue({super.key, required this.image});
 
+  final String image;
+  EditVenue({super.key, required this.image});
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(myvenuecontroller());
     TextEditingController venueName = TextEditingController();
     TextEditingController location = TextEditingController();
     TextEditingController numberGuests=TextEditingController();
@@ -89,35 +95,14 @@ class EditVenue extends StatelessWidget {
             child:Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                SizedBox(
-                  width: double.infinity  ,
-
-                  child: TextField(
-                    controller: venueName,
-                    decoration: InputDecoration(
-                        labelText: "Venue Name",
-                        labelStyle: getTextStyle(
-                          fontSize: 12,
-                          fontWeight: FontWeight.w500,
-                          color: Color(0xff767676),
-                        )
-                    ),
-                  ),
+                CustomInputField(
+                  controller: venueName,
+                  label: "Venue Name",
                 ),
                 SizedBox(height: 16,),
-                SizedBox(
-                 width: double.infinity,
-                  child: TextField(
-                    controller: location,
-                    decoration: InputDecoration(
-                        labelText: "Location",
-                        labelStyle: getTextStyle(
-                          fontSize: 12,
-                          fontWeight: FontWeight.w500,
-                          color: Color(0xff767676),
-                        )
-                    ),
-                  ),
+                CustomInputField(
+                  controller: location,
+                  label: "Location",
                 ),
                 SizedBox(height: 16,),
                 Text('Select From Map',style: getTextStyle(
@@ -143,20 +128,9 @@ class EditVenue extends StatelessWidget {
                   ),
                 ),
                 SizedBox(height: 16,),
-                SizedBox(
-                  width: double.infinity ,
-
-                  child: TextField(
-                    controller: numberGuests,
-                    decoration: InputDecoration(
-                        labelText: "Number of Guests",
-                        labelStyle: getTextStyle(
-                          fontSize: 12,
-                          fontWeight: FontWeight.w500,
-                          color: Color(0xff767676),
-                        )
-                    ),
-                  ),
+                CustomInputField(
+                  controller: numberGuests,
+                  label: "Number of Guests",
                 ),
                 SizedBox(height: 15,),
                 Text('Amenities',
@@ -166,119 +140,35 @@ class EditVenue extends StatelessWidget {
                       color: const Color(0xff333333),
                     )),
                 SizedBox(height: 5,),
-                const SizedBox(height: 16),
                 Row(
                   children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Container(
-                        height: screenHeight*0.03,
-                        width: screenWidth*0.15,
-                        decoration: BoxDecoration(
-
-                          color: Color(0xffFBF7EB),
-                            borderRadius: BorderRadius.circular(32),
-                            border: Border.all(
-                              width: 1,
-                              color: Color(0xffC0C0C0),
-                            )
-                        ),child: Row(
-
-                        children: [
-                          SizedBox(width: 5,),
-                          Icon(Icons.wifi,size: 14,color: Color(0xffD4AF37),),
-                          SizedBox(width: 8),
-                          Text('Wifi',style: getTextStyle(
-                              fontSize: 14,
-                              fontWeight: FontWeight.w400,
-                              color: Color(0xff333333)
-                          ),)
-                        ],
-                      ),
-                      ),
+                    FacilityTag(
+                      width: screenWidth * 0.18,
+                      icon: Icons.wifi,
+                      label: 'Wifi',
                     ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Container(
-                        height: screenHeight*0.03,
-                        width: screenWidth*0.22,
-                        decoration: BoxDecoration(
-                            color: Color(0xffFBF7EB),
-                            borderRadius: BorderRadius.circular(32),
-                            border: Border.all(
-                              width: 1,
-                              color: Color(0xffC0C0C0),
-                            )
-                        ),child: Row(
 
-                        children: [
-                          SizedBox(width: 5,),
-                          Icon(Icons.wifi,size: 14,color: Color(0xffD4AF37),),
-                          SizedBox(width: 8),
-                          Text('Parking',style: getTextStyle(
-                              fontSize: 14,
-                              fontWeight: FontWeight.w400,
-                              color: Color(0xff333333)
-                          ),)
-                        ],
-                      ),
-                      ),
+                    SizedBox(width: 13,),
+                    FacilityTag(
+                      width: screenWidth * 0.24,
+                      icon: Icons.wifi,
+                      label: 'Parking',
                     ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Container(
-                        height: screenHeight*0.03,
-                        width: screenWidth*0.15,
-                        decoration: BoxDecoration(
-                            color: Color(0xffFBF7EB),
-                            borderRadius: BorderRadius.circular(32),
 
-                            border: Border.all(
-                              width: 1,
-                              color: Color(0xffC0C0C0),
-                            )
-                        ),child: Row(
-
-                        children: [
-                          SizedBox(width: 5,),
-                          Icon(Icons.wifi,size: 14,color: Color(0xffD4AF37),),
-                          SizedBox(width: 8),
-                          Text('AC',style: getTextStyle(
-                              fontSize: 14,
-                              fontWeight: FontWeight.w400,
-                              color: Color(0xff333333)
-                          ),)
-                        ],
-                      ),
-                      ),
+                    SizedBox(width: 13,),
+                    FacilityTag(
+                      width: screenWidth * 0.16,
+                      icon: Icons.wifi,
+                      label: 'AC',
                     ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Container(
-                        height: screenHeight*0.03,
-                        width: screenWidth*0.15,
-                        decoration: BoxDecoration(
-                            color: Color(0xffFBF7EB),
-                            borderRadius: BorderRadius.circular(32),
-                            border: Border.all(
-                              width: 1,
-                              color: Color(0xffC0C0C0),
-                            )
-                        ),child: Row(
 
-                        children: [
-                          SizedBox(width: 5,),
-                          Icon(Icons.wifi,size: 14,color: Color(0xffD4AF37),),
-                          SizedBox(width: 8),
-                          Text('Pool',style: getTextStyle(
-                              fontSize: 14,
-                              fontWeight: FontWeight.w400,
-                              color: Color(0xff333333)
-                          ),)
-                        ],
-                      ),
-                      ),
+                    SizedBox(width: 13,),
+                    FacilityTag(
+                      width: screenWidth * 0.19,
+                      icon: Icons.wifi,
+                      label: 'Pool',
                     ),
+
 
                   ],
                 ),
@@ -291,32 +181,120 @@ class EditVenue extends StatelessWidget {
                     )),
                 SizedBox(height: 16),
                 Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
                   children: [
-                    Container(
-                      height:screenHeight*0.028,
-                      width: screenWidth*0.28,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(34),
-                        color: Color(0xffFFFFFFF),
+                  CustomShapeTag(label: 'Storage & lighting',  height: screenHeight * 0.05,
+                    width: screenWidth * 0.38,
+                    icon:Icon(Icons.wifi, size: 15, color: Color(0xffD4AF37)),
 
-                        boxShadow: [
-                          BoxShadow(
-                            color:Color(0xff000000).withOpacity(.6),//sersr
-                            blurRadius: 10,
-                            offset: Offset(0, 3),
-                          )
-                        ]
-                      ),
-                      child: Row(
-                        children: [
-                          Icon(Icons.wifi,size:16 ,color:Color(0xff333333) ,),
-                          SizedBox(width: 8,),
-
-                        ],
-                      ),
-                    )
+                  ),
+                    SizedBox(width: .2,),
+                    CustomShapeTag( icon:Icon(Icons.wifi, size: 15, color: Color(0xffD4AF37)),label: 'Sound System', height: screenHeight * 0.05, width:screenWidth * 0.38),
                   ],
-                )
+                ),
+                SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    CustomShapeTag(label: 'Barservices',  height: screenHeight * 0.05, width:screenWidth*0.27,icon:Icon(Icons.wifi, size: 15, color: Color(0xffD4AF37)),),
+                    SizedBox(width: .2,),
+                    CustomShapeTag(label: 'Elecator', height: screenHeight * 0.05, width: screenHeight * 0.14,icon:Icon(Icons.wifi, size: 15, color: Color(0xffD4AF37)),),
+                    CustomShapeTag(label: 'Restroom', height: screenHeight * 0.05, width: screenHeight * 0.15,icon:Icon(Icons.wifi, size: 15, color: Color(0xffD4AF37)),),
+                  ],
+                ),
+                SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    CustomShapeTag(label: 'Event Staff',  height: screenHeight * 0.05,
+                      width: screenWidth * 0.30,
+                      icon:Icon(Icons.wifi, size: 15, color: Color(0xffD4AF37)),
+
+                    ),
+                    SizedBox(width: .2,),
+                    CustomShapeTag( icon:Icon(Icons.wifi, size: 15, color: Color(0xffD4AF37)),label: 'Sound System', height: screenHeight * 0.05, width:screenWidth * 0.38),
+                  ],
+                ),
+                SizedBox(height: 19,),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: Container(
+                    width: 48,
+                    height: 40,
+                 decoration: BoxDecoration(
+                   color:Color(0xff003366),
+                   shape: BoxShape.circle
+                 ),
+                    child: Center(
+                      child: Icon(Icons.add,color:Colors.white,size: 24,),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20,),
+                Text('Decoration',style: getTextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w700,
+                  color: Color(0xff333333),
+                ),),
+                SizedBox(height: 16),
+                SizedBox(
+                  height: screenHeight * 0.249,
+                  width: double.infinity,
+                  child: Image.asset(ImagePath.venueview, fit: BoxFit.cover),
+                ),
+                SizedBox(height: 16,),
+                Text(
+                  'Table Shape',
+                  style: getTextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                    color: Color(0xff333333),
+                  ),
+                ),
+                SizedBox(height: 16),
+                Obx(() {
+                  final shapes = controller.tableShapeOptions;
+
+                  return Wrap(
+                    spacing: 5, // checkbox গুলোর মাঝে horizontal gap
+                    runSpacing: 4, // new line হলে vertical gap
+                    children: shapes.map((shape) {
+                      final isSelected = controller.selectedShapes.contains(shape);
+                      return Row(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Checkbox(
+                            value: isSelected,
+                            onChanged: (_) => controller.toggleShape(shape),
+                            activeColor: Color(0xff003366),
+                          ),
+                          Text(
+                            shape,
+                            style: getTextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w400,
+                              color: Color(0xff333333),
+                            ),
+                          ),
+                        ],
+                      );
+                    }).toList(),
+                  );
+                })
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
               ],
---
+++ lib/features/role/venue_owner/myvenue/widget/facilty_Tag.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:blinqo/core/common/styles/global_text_style.dart';
+
+class FacilityTag extends StatelessWidget {
+  final double width;
+  final IconData icon;
+  final String label;
+
+  const FacilityTag({
+    super.key,
+    required this.width,
+    required this.icon,
+    required this.label,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      decoration: BoxDecoration(
+        color: const Color(0xffFBF7EB),
+        borderRadius: BorderRadius.circular(32),
+        border: Border.all(
+          width: 1,
+          color: const Color(0xffC0C0C0),
+        ),
+      ),
+      child: Row(
+        children: [
+          const SizedBox(width: 6),
+          Icon(icon, size: 14, color: const Color(0xffD4AF37)),
+          const SizedBox(width: 8),
+          Text(
+            label,
+            style: getTextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w400,
+              color: const Color(0xff333333),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
